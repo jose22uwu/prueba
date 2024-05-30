2694ad998aed97554af260550a625d0bc0777994
jose vargas <jose.vargaslopez@ucr.ac.cr>
2024-05-29 20:37:18 -0600

    hola

--- app/Http/Controllers/taskController.php
+++ app/Http/Controllers/taskController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class taskController extends Controller
 {
-    public function index(){
+    public function index(){ //comento aqui algo para el git chill
         $task = Task::all();
         return view('tasksview',compact('task')) ; //el compact sirve para preparar y poder usar lo solicitado a la vista, es una forma de hacerlo
     }
